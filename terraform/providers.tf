929a69236ab8a981413a79e1137e63964502bd54
Akas Patranobis <akaspatranobis@yahoo.com>
2024-05-26 00:42:36 +0530

    updates

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,5 +11,5 @@ provider "google" {
   # Configuration options
   project = var.project_id
   region = var.region
-  credentials = "gcp-key.json"
+  credentials = file("${path.module}/gcp-key.json")
 }
